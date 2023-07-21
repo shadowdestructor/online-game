0515cc00479b9d89e61444b17d8d2bc16af0d634
shadowdestructor <yasinyildirim27@outlook.com>
2023-07-21 16:58:03 +0300

    netcode

--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -1,28 +1,5 @@
 using UnityEngine;
-public class ConnectionManager : MonoBehaviour
-{
-    /*public GameObject playerPrefab; // Prefabınızı bu alanda atayın.
 
-    void Start()
-    {
-        PhotonNetwork.ConnectUsingSettings(); // Photon'a bağlan
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        // Photon sunucusuna bağlandığında çağırılacak
-        Debug.Log("Connected to Photon Server");
-
-        // Oyuncuyu oluştur ve ağda senkronize et
-        SpawnPlayer();
-    }
-
-    private void SpawnPlayer()
-    {
-        // Oyuncunun pozisyonunu belirleyin
-        Vector3 spawnPosition = new Vector3(0f, 0f, 0f);
-
-        // Play objenizi prefab olarak ağda oluşturun
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
-    }*/
+public class ConnectionManager : MonoBehaviour {
+    
 }
